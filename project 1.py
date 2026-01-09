59521d7883f394e0c9a0eb639121939943309b28
Rafiul Islam <rafiulislam7351@gmail.com>
2026-01-09 22:57:32 +0600

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,27 @@
+import random
+
+computer = -random.choice([1, -1, 0])
+player = input("Enter your choice(Rock,Paper,Scissors):").capitalize()
+
+playerDict = {"Rock": 1, "Paper": -1, "Scissors": 0}
+computerDict = {1: "Rock", -1: "Paper", 0: "Scissors"}
+
+print("Computer's choice =", computerDict[computer])
+player_number = playerDict[player]
+
+if computer == -1 and player_number == 1:
+    print("You Lose!")
+elif computer == -1 and player_number == 0:
+    print("You Win!")
+elif computer == 1 and player_number == 0:
+    print("You Lose!")
+elif computer == 1 and player_number == -1:
+    print("You Win!")
+elif computer == 0 and player_number == 1:
+    print("You Win!")
+elif computer == 0 and player_number == -1:
+    print("You Lose!")
+elif computer == player_number:
+    print("Draw (>_<)")
+else:
+    print("Keep trying!")
